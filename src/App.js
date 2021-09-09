7f0274143a65917b889ed86591ce5e639546b196
malakkhasawneh2 <malakkhasawneh2@gmail.com>
2021-09-09 16:39:39 +0300

    lap8Finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import axios from 'axios';
 import React from 'react';
+import Weather from './components/Weather';
+import Movie from './components/Movie';
+
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
@@ -16,20 +19,31 @@ class App extends React.Component {
       disPlayWeatherError: false,
       weatherArray: [],
       mapDisplay: false,
+      disPlayMoviesError: false,
+      moviesDataArray: [],
+      Movies: false,
     }
   }
 
-  getWeatherData = async (lat,lon) => {
-    let WeatherURL = `https://city-lap7.herokuapp.com/weather?lat=${lat}&lon=${lon}`;
 
 
-  try {
+
+
+  getWeatherData = async (cityName) => {
+    // event.preventDefault();
+
+    // let cityName = event.target.cityName.value;
+//http://localhost:3333/weather?cityName=Amman
+    let WeatherURL = `https://city-lap7.herokuapp.com/weather?cityName=${cityName}`;
+
+
+    try {
       let WeatherData = await axios.get(WeatherURL)
-        this.setState({
-          weatherArray: WeatherData.data,
-          weather:true
-        })
-        console.log(this.state.weatherArray);
+      this.setState({
+        weatherArray: WeatherData.data,
+        weather: true
+      })
+      console.log(this.state.weatherArray);
     }
     catch {
       this.setState({
@@ -39,10 +53,13 @@ class App extends React.Component {
   }
 
   getCityData = async (event) => {
-    event.preventDefault();
     let cityName = event.target.cityName.value;
+    event.preventDefault();
+    this.getWeatherData(cityName)
+    this.getMoviesData(cityName)
     // this.getWeatherData(cityName);
-    let URL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION}&q=${cityName}&format=json`;
+    const myKey = "pk.43fed3791d35ddb76aa14f749c6d3080";
+    let URL = `https://eu1.locationiq.com/v1/search.php?key=${myKey}&q=${cityName}&format=json`;
 
     try {
       let Data = await axios.get(URL);
@@ -51,53 +68,94 @@ class App extends React.Component {
         lat: Data.data[0].lat,
         display_name: Data.data[0].display_name,
         mapDisplay: true
-
+    
       })
-      this.getWeatherData(Data.data[0].lat,Data.data[0].lon)
+      // this.getWeatherData(Data.data[0].lat, Data.data[0].lon)
     }
     catch {
       this.setState({
         disPlayError: true
+        
+      })
+    }
+
+  }
+
+  getMoviesData = async (cityName) => {
+    // event.preventDefault();
+    // let cityName = event.target.cityName.value;
+    //http://localhost:3333/movies?cityName=angel
+    let MovieURL = `https://city-lap7.herokuapp.com/movies?cityName=${cityName}`;
+    try {
+      let moviesData = await axios.get(MovieURL)
+      console.log(moviesData);
+      this.setState({
+        moviesDataArray: moviesData.data,
+        Movies: true
+      })
+      // this.getCityData(cityName);
+      console.log(this.state.moviesDataArray);
+    }
+    catch {
+      this.setState({
+        disPlayMoviesError: true
+
       })
     }
   }
 
 
-  render() {
+render() {
 
+//  getMoviesData;
+//   getWeatherData;
 
+  return (
 
-    return (
+    <>
 
-        <>
+      <h1>City Location</h1>
+      <form onSubmit={this.getCityData}>
+        <input type='text' name='cityName' placeholder='Enter city name' />
+        <button type='submit'>Location</button>
+      </form>
 
-        <h1>City Location</h1>
-        <form onSubmit={this.getCityData}>
-          <input type='text' name='cityName' placeholder='Enter city name' />
-          <button type='submit'>Location</button>
-        </form>
+      {this.state.mapDisplay && (<div>
         <p>City name:{this.state.display_name}</p>
         <p>Latitude:{this.state.lat}</p>
         <p>Longitude:{this.state.lon}</p>
-         {this.state.mapDisplay &&
-         <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION}&center=${this.state.lat},${this.state.lon}&zoom=${1-18}`} alt='map' />}
-        {this.state.disPlayError && <p>something Error</p>}
-        {this.state.weather && this.state.weatherArray.map(item =>{
-          return(
-          <>
-           <p>Date: {item.date}</p>
-           <p>description: {item.description}</p>
-          </>
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.43fed3791d35ddb76aa14f749c6d3080&center=${this.state.lat},${this.state.lon}&zoom=${1 - 18}`} alt='map' /></div>)}
+      {this.state.disPlayError && <p>something Error</p>}
 
-)
-})}
-    {this.state.disPlayWeatherError&&<p >something Error</p>}    
-      </>
-    );
-  }
+      {this.state.mapDisplay && (< Weather
+        weather={this.state.weatherArray.map(item => {
+          return (
+
+            <>
+
+              <p>Date: {item.date}</p>
+              <p>description: {item.description}</p>
+
+            </>);
+        })} />)}
+
+      {this.state.mapDisplay && (<Movie
+        movie={this.state.moviesDataArray.map(item => {
+          return (
+            <>
+            <img src = {item.image_url}/>
+              <p>Title: {item.title}</p>
+              <p>Overview: {item.overview}</p>
+              <p>Average_votes: {item.average_votes}</p>
+              <p>Total_votes: {item.total_votes}</p>
+              <p>Popularity: {item.popularity}</p>
+              <p>Released_on: {item.released_on}</p>
+            </>);
+        })} />)}
+    </>)
+}
 }
 
 export default App;
-
 
 
---
+++ src/components/Movie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Movie extends React.Component{
+    render()
+    {
+        return(
+            <>
+         {this.props.movie }
+
+
+            </>
+        )
+    }
+}
+export default Movie;
